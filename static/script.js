ee9b21684c2d22a7198d0bf18d8652004e03c377
Jake Wallin <Jakewallin42@gmail.com>
2025-03-06 08:14:34 -0500

    Upgraded UI with modern styling

---
+++ static/script.js
@@ -0,0 +1,25 @@
+function addConstraint() {
+    let div = document.createElement("div");
+    div.innerHTML = `
+        <select name="constraint_column[]">
+            <option value="">Select Column</option>
+            <option value="column1">Column 1</option>
+            <option value="column2">Column 2</option>
+        </select>
+        <select name="constraint_operator[]">
+            <option value="=">=</option>
+            <option value="<"><</option>
+            <option value=">">></option>
+            <option value="<="><=</option>
+            <option value=">=">>=</option>
+            <option value="!=">!=</option>
+        </select>
+        <input type="text" name="constraint_value[]" placeholder="Enter Value">
+        <button type="button" onclick="removeConstraint(this)">❌ Remove</button>
+    `;
+    document.getElementById("constraints").appendChild(div);
+}
+
+function removeConstraint(button) {
+    button.parentNode.remove();
+}
